867e85eac281ee46626dfcf1001b7bb78fbbb08d
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-10-10 16:30:34 -0400

    Basic mobile menu configured

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,35 +1,51 @@
+import { useState } from 'react';
 import Logo from '../../assets/logo1.png';
 import { Bars3Icon } from '@heroicons/react/24/solid';
+import { XMarkIcon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+    const toggleState = () => {
+        setIsNavbarOpen(!isNavbarOpen);
+    };
+
     return (
-        <nav className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+        <nav className="fixed w-full h-16 flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
             <div>
                 <img src={Logo} alt="Logo Image" className="w-12" />
             </div>
             {/* menu */}
-            <div>
-                <ul className="flex">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Experience</li>
-                    <li>Work</li>
-                    <li className="font-raleway">Contact</li>
-                </ul>
-            </div>
+            <ul className="hidden sm:flex">
+                <li className="px-4 cursor-pointer">Home</li>
+                <li className="px-4 cursor-pointer">About</li>
+                <li className="px-4 cursor-pointer">Experience</li>
+                <li className="px-4 cursor-pointer">Work</li>
+                <li className="px-4 cursor-pointer">Contact</li>
+            </ul>
             {/* hamburger */}
-            <div className="hidden">
-                <Bars3Icon className="h-6 w-6 text-gray-200" />
+            <div onClick={toggleState} className="sm:hidden z-10">
+                {isNavbarOpen ? (
+                    <XMarkIcon className="h-6 w-6 text-gray-200" />
+                ) : (
+                    <Bars3Icon className="h-6 w-6 text-gray-200" />
+                )}
             </div>
             {/* Mobile menu */}
-            <ul className="hidden">
-                <li>Home</li>
-                <li>About</li>
-                <li>Experience</li>
-                <li>Work</li>
-                <li>Contact</li>
+            <ul
+                className={
+                    !isNavbarOpen
+                        ? 'hidden'
+                        : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+                }
+            >
+                <li className="py-6 text-4xl cursor-pointer">Home</li>
+                <li className="py-6 text-4xl cursor-pointer">About</li>
+                <li className="py-6 text-4xl cursor-pointer">Experience</li>
+                <li className="py-6 text-4xl cursor-pointer">Work</li>
+                <li className="py-6 text-4xl cursor-pointer">Contact</li>
             </ul>
             {/* Social Icons */}
+            <div className="hidden"></div>
         </nav>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                raleway: ['Raleway', 'sans'],
+                sans: ['Raleway', 'sans'],
             },
         },
     },
