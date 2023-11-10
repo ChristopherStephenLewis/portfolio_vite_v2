5648514c493962ca91064b8160227d0e598f290f
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-11-10 13:19:43 -0500

    Updated portfolio with new resume and YouTube clone project

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,116 +1,116 @@
-import { useState } from 'react';
-import { Bars3Icon } from '@heroicons/react/24/solid';
-import { XMarkIcon } from '@heroicons/react/24/solid';
-import { FaLinkedin, FaGithub } from 'react-icons/fa';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from '../../assets/logo-v3.svg';
-import Resume from '../../assets/files/Christopher_Lewis_Resume-10222023.pdf';
+import { useState } from "react";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Logo from "../../assets/logo-v3.svg";
+import Resume from "../../assets/files/Christopher_Lewis_Resume.pdf";
 
 const Navbar = () => {
-    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
-    const toggleState = () => {
-        setIsNavbarOpen(!isNavbarOpen);
-    };
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const toggleState = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
 
-    return (
-        <nav className="fixed w-full h-16 flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-10">
-            <div>
-                <img src={Logo} alt="Logo Image" className="w-16" />
-            </div>
-            {/* menu */}
-            <ul className="hidden sm:flex">
-                <li className="px-4">
-                    <a href="#home" className="hover:text-pink-600">
-                        Home
-                    </a>
-                </li>
-                <li className="px-4">
-                    <a href="#about" className="hover:text-pink-600">
-                        About
-                    </a>
-                </li>
-                <li className="px-4">
-                    <a href="#skills" className="hover:text-pink-600">
-                        Skills
-                    </a>
-                </li>
-                <li className="px-4">
-                    <a href="#work" className="hover:text-pink-600">
-                        Work
-                    </a>
-                </li>
-                <li className="px-4">
-                    <a href="#contact" className="hover:text-pink-600">
-                        Contact
-                    </a>
-                </li>
-            </ul>
-            {/* hamburger */}
-            <div onClick={toggleState} className="z-10 sm:hidden">
-                {isNavbarOpen ? (
-                    <XMarkIcon className="w-6 h-6 text-gray-200" />
-                ) : (
-                    <Bars3Icon className="w-6 h-6 text-gray-200" />
-                )}
-            </div>
-            {/* Mobile menu */}
-            <ul
-                className={
-                    !isNavbarOpen
-                        ? 'hidden'
-                        : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
-                }
+  return (
+    <nav className="fixed w-full h-16 flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-10">
+      <div>
+        <img src={Logo} alt="Logo Image" className="w-16" />
+      </div>
+      {/* menu */}
+      <ul className="hidden sm:flex">
+        <li className="px-4">
+          <a href="#home" className="hover:text-pink-600">
+            Home
+          </a>
+        </li>
+        <li className="px-4">
+          <a href="#about" className="hover:text-pink-600">
+            About
+          </a>
+        </li>
+        <li className="px-4">
+          <a href="#skills" className="hover:text-pink-600">
+            Skills
+          </a>
+        </li>
+        <li className="px-4">
+          <a href="#work" className="hover:text-pink-600">
+            Work
+          </a>
+        </li>
+        <li className="px-4">
+          <a href="#contact" className="hover:text-pink-600">
+            Contact
+          </a>
+        </li>
+      </ul>
+      {/* hamburger */}
+      <div onClick={toggleState} className="z-10 sm:hidden">
+        {isNavbarOpen ? (
+          <XMarkIcon className="w-6 h-6 text-gray-200" />
+        ) : (
+          <Bars3Icon className="w-6 h-6 text-gray-200" />
+        )}
+      </div>
+      {/* Mobile menu */}
+      <ul
+        className={
+          !isNavbarOpen
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li onClick={toggleState} className="py-6 text-4xl cursor-pointer">
+          <a href="#home">Home</a>
+        </li>
+        <li onClick={toggleState} className="py-6 text-4xl cursor-pointer">
+          <a href="#about">About</a>
+        </li>
+        <li onClick={toggleState} className="py-6 text-4xl cursor-pointer">
+          <a href="#skills">Skills</a>
+        </li>
+        <li onClick={toggleState} className="py-6 text-4xl cursor-pointer">
+          <a href="#work">Work</a>
+        </li>
+        <li onClick={toggleState} className="py-6 text-4xl cursor-pointer">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      {/* Social Icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-40 h-16 p-4 flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500 border border-gray-700 rounded-tr-lg hover:rounded-lg">
+            <a
+              className="flex items-center justify-between w-full text-gray-300"
+              href="https://www.linkedin.com/in/christopher-s-lewis/"
+              target="_blank"
             >
-                <li onClick={toggleState} className="py-6 text-4xl cursor-pointer">
-                    <a href="#home">Home</a>
-                </li>
-                <li onClick={toggleState} className="py-6 text-4xl cursor-pointer">
-                    <a href="#about">About</a>
-                </li>
-                <li onClick={toggleState} className="py-6 text-4xl cursor-pointer">
-                    <a href="#skills">Skills</a>
-                </li>
-                <li onClick={toggleState} className="py-6 text-4xl cursor-pointer">
-                    <a href="#work">Work</a>
-                </li>
-                <li onClick={toggleState} className="py-6 text-4xl cursor-pointer">
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
-            {/* Social Icons */}
-            <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
-                <ul>
-                    <li className="w-40 h-16 p-4 flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500 border border-gray-700 rounded-tr-lg hover:rounded-lg">
-                        <a
-                            className="flex items-center justify-between w-full text-gray-300"
-                            href="https://www.linkedin.com/in/christopher-s-lewis/"
-                            target="_blank"
-                        >
-                            LinkedIn <FaLinkedin size={30} />
-                        </a>
-                    </li>
-                    <li className="w-40 h-16 p-4 flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-900 border-gray-700 border hover:rounded-lg">
-                        <a
-                            className="flex items-center justify-between w-full text-gray-300"
-                            href="https://github.com/ChristopherStephenLewis"
-                            target="_blank"
-                        >
-                            GitHub <FaGithub size={30} />
-                        </a>
-                    </li>
-                    <li className="w-40 h-16 p-4 flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-sky-800 border border-gray-700 rounded-br-lg hover:rounded-lg">
-                        <a
-                            className="flex items-center justify-between w-full text-gray-300"
-                            href={Resume}
-                            target="_blank"
-                        >
-                            Resume <BsFillPersonLinesFill size={30} />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-40 h-16 p-4 flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-900 border-gray-700 border hover:rounded-lg">
+            <a
+              className="flex items-center justify-between w-full text-gray-300"
+              href="https://github.com/ChristopherStephenLewis"
+              target="_blank"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-40 h-16 p-4 flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-sky-800 border border-gray-700 rounded-br-lg hover:rounded-lg">
+            <a
+              className="flex items-center justify-between w-full text-gray-300"
+              href={Resume}
+              target="_blank"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/components/pages/Work.tsx
+++ src/components/pages/Work.tsx
@@ -1,58 +1,69 @@
-import WorkImg from '../../assets/workImg.jpeg';
-import RealEstate from '../../assets/realestate.jpg';
-import FaceRecog from '../../assets/face-recognition-project.png';
+import FaceRecog from "../../assets/face-recognition-project.png";
+import YTLogo2 from "../../assets/yt-logo2.webp";
 
 const workData = [
-    {
-        img: FaceRecog,
-        title: 'Face Recognition Website',
-        demoLink: 'https://christopherstephenlewis.github.io/facerecognitionwebsite/',
-        codeLink: 'https://github.com/ChristopherStephenLewis/facerecognitionwebsite',
-    },
-    { img: RealEstate, title: 'React JS Application', demoLink: '/', codeLink: '/' },
-    { img: WorkImg, title: 'React JS Application', demoLink: '/', codeLink: '/' },
+  {
+    img: FaceRecog,
+    title: "Face Recognition Website",
+    demoLink:
+      "https://christopherstephenlewis.github.io/facerecognitionwebsite/",
+    codeLink:
+      "https://github.com/ChristopherStephenLewis/facerecognitionwebsite",
+  },
+  {
+    img: YTLogo2,
+    title: "YouTube Clone",
+    demoLink: "https://yt-web-client-pziekzs4da-uc.a.run.app/",
+    codeLink: "https://github.com/ChristopherStephenLewis/youtube-clone",
+  },
 ];
 
 const Work = () => {
-    return (
-        <div className="bg-[#0a192f] w-full md:h-screen text-gray-300 scroll-mt-12" id="work">
-            <div className="flex flex-col justify-center w-full h-full max-w-4xl p-4 mx-auto">
-                <div className="pb-8">
-                    <p className="inline text-4xl font-bold border-b-4 border-pink-600">Work</p>
-                    <p className="py-4 text-xl">
-                        Check out some of my <span className="text-[#00a0ea]">recent work</span>:
-                    </p>
-                </div>
-                <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
-                    {workData.map((work, index) => (
-                        <article
-                            key={index}
-                            style={{ backgroundImage: `url(${work.img})` }}
-                            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-                        >
-                            <div className="flex flex-col opacity-0 group-hover:opacity-90 ">
-                                <p className="text-2xl font-bold tracking-wider text-center text-white">
-                                    {work.title}
-                                </p>
-                                <div className="pt-8 text-center">
-                                    <a href={work.demoLink} className="m-2" target="_blank">
-                                        <button className="px-4 py-3 text-lg font-bold text-center text-gray-700 duration-200 bg-white rounded-lg hover:scale-110">
-                                            Demo
-                                        </button>
-                                    </a>
-                                    <a href={work.codeLink} className="m-2" target="_blank">
-                                        <button className="px-4 py-3 text-lg font-bold text-center text-gray-700 duration-200 bg-white rounded-lg hover:scale-110">
-                                            Code
-                                        </button>
-                                    </a>
-                                </div>
-                            </div>
-                        </article>
-                    ))}
-                </div>
-            </div>
+  return (
+    <div
+      className="bg-[#0a192f] w-full md:h-screen text-gray-300 scroll-mt-12"
+      id="work"
+    >
+      <div className="flex flex-col justify-center w-full h-full max-w-4xl p-4 mx-auto">
+        <div className="pb-8">
+          <p className="inline text-4xl font-bold border-b-4 border-pink-600">
+            Work
+          </p>
+          <p className="py-4 text-xl">
+            Check out some of my{" "}
+            <span className="text-[#00a0ea]">recent work</span>:
+          </p>
         </div>
-    );
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+          {workData.map((work, index) => (
+            <article
+              key={index}
+              style={{ backgroundImage: `url(${work.img})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              <div className="flex flex-col opacity-0 group-hover:opacity-90 ">
+                <p className="text-2xl font-bold tracking-wider text-center text-white">
+                  {work.title}
+                </p>
+                <div className="pt-8 text-center">
+                  <a href={work.demoLink} className="m-2" target="_blank">
+                    <button className="px-4 py-3 text-lg font-bold text-center text-gray-700 duration-200 bg-white rounded-lg hover:scale-110">
+                      Demo
+                    </button>
+                  </a>
+                  <a href={work.codeLink} className="m-2" target="_blank">
+                    <button className="px-4 py-3 text-lg font-bold text-center text-gray-700 duration-200 bg-white rounded-lg hover:scale-110">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
